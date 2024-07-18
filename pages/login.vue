da27311dafe2ec3e11c5a326e718c87c61b26433
Wouter <wouter@arlanet.com>
2024-07-18 16:45:50 +0200

    feat: Update login page with GitHub login button

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="prose w-full max-w-2xl h-9">
+		<h1>Log in to {{ title }}</h1>
+		<button
+			class="bg-blue-800 text-white font-bold py-2 px-4 transition-all rounded hover:scale-90 hover:bg-blue-500"
+		>
+			Log in with Github
+		</button>
+	</div>
+</template>
+
+<script setup>
+const { title } = useCourse();
+</script>
